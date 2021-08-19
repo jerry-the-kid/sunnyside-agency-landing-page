e0eba0dac2193af9e78179a9b16d3bf2fcd0f7ba
jerry-the-kid <69315874+jerry-the-kid@users.noreply.github.com>
2021-08-19 22:56:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,19 @@ navChoice.addEventListener('click', function (e) {
   if (window.screen.width < 750) toggleMenu();
 });
 
+gsap.to('.navbar', {
+  scrollTrigger: {
+    trigger: '#info--1',
+    toggleActions: 'play play reset reset',
+    start: 'top center',
+    end: 'top center',
+  },
+  position: 'fixed',
+  top: 0,
+  backgroundColor: `rgba(136, 255, 247, 0.5)`,
+  padding: '2rem 4rem',
+});
+
 const animate = function () {
   const setOption = function (trigger, position = 'top', viewport = 'center') {
     return {
@@ -39,36 +52,23 @@ const animate = function () {
   const setImgOption = function (trigger, translate) {
     return {
       scrollTrigger: setOption(`${trigger}`),
-      x: `${translate}`,
+      x: `${translate}px`,
       opacity: 0,
       duration: 2,
     };
   };
 
-  gsap.to('.navbar', {
-    scrollTrigger: {
-      trigger: '#info--1',
-      toggleActions: 'play play reset none',
-      start: 'top center',
-      end: 'top center',
-    },
-    position: 'fixed',
-    top: 0,
-    backgroundColor: `rgba(136, 255, 247, 0.5)`,
-    padding: '2rem 4rem',
-  });
+  gsap.from('.info__img--1', setImgOption('.info__img--1', '150'));
 
-  gsap.from('.info__img--1', setImgOption('.info__img--1', '40%'));
+  gsap.from('.service__right', setImgOption('.service__right', '-150'));
+
+  gsap.from('.service__left', setImgOption('.service__left', '150'));
+
+  gsap.from('.info__img--2', setImgOption('.info__img--2', '-150'));
 
   gsap.from('.info__content--1', setContentOption('.info__content-box--1'));
 
   gsap.from('.info__content--2', setContentOption('.info__content-box--2'));
-
-  gsap.from('.service__right', setImgOption('.service__right', '-40%'));
-
-  gsap.from('.service__left', setImgOption('.service__left', '40%'));
-
-  gsap.from('.info__img--2', setImgOption('.info__img--2', '-40%'));
 
   gsap.from('.testimonial', {
     scrollTrigger: setOption('.testimonials'),
